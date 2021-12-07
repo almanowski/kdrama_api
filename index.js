3d4ccc66c32208f3500686da070c17ec92a552e2
Anna Manowski <anna.l.manowski@gmail.com>
2021-12-07 12:18:53 +0100

    Update requests

--- index.js
+++ index.js
@@ -15,14 +15,19 @@ const KDramas= Models.KDrama,
 
 
 // GENERAL
+// Allows you to read the “body” of HTTP requests within your request handlers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 // Logs time, method, url path, response code, numb of charac of res that was sent back
 app.use(morgan('common'));
 
-// Allows you to read the “body” of HTTP requests within your request handlers
-app.use(bodyParser.json());
-
 // Serves all static files in public folder
 app.use(express.static('public'));
+
+// Conect to DB
+mongoose.connect('mongodb://127.0.0.1:27017/kDramaDB',
+    {useNewUrlParser: true, useUnifiedTopology: true});
 
 // CONTENT
 app.get('/', (req, res) => {
@@ -31,52 +36,156 @@ app.get('/', (req, res) => {
 
 // Displays dramas
 app.get('/korean-dramas', (req, res) => {
-    res.json(topKoreanDramas);
+    KDramas.find()
+    .populate('Genre')
+    .then((kDramas) => {
+        res.status(201).json(kDramas);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
 });
 
 // Displays one drama
 app.get('/korean-dramas/:title', (req, res) => {
-    res.json(topKoreanDramas.find((drama) => {
-        return drama.title === req.params.title;
-    }));
+    KDramas.findOne({Title: req.params.title})
+    .populate('Genre')
+    .then((kDramas) => {
+        res.status(201).json(kDramas);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+    });
 });
 
 // Displays drama from a certain genre
-app.get('/genres/:genre', (req, res) => {
-    res.send('Sucesscul GET request - Returning dramas by genre');
+app.get('/genres/:name', (req, res) => {
+  KDramas.find({Name: req.params.name})
+  .populate('Genre')
+  .then((kDramas) => {
+      res.status(201).json(kDramas);
+  })
+  .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+  });
 });
 
 // Displays one director
 app.get('/directors/:name', (req, res) => {
-    res.send('Sucesscul GET request - Returning drama director');
+  KDramas.findOne({'Director.Name': req.params.name},
+  {'Director.Name': 1, 'Director.Bio':1, 'Director.Birth':1, _id:0})
+  .then((kDramas) => {
+      res.status(201).json(kDramas);
+  })
+  .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+  });
 });
 
 
 //USER
-// Adds data for a new user
+// Adds data for a new user !!!!! Doesn't show Birthday!!!!!!!!!!!!
 app.post('/users', (req, res) => {
-    res.send('Sucesscul POST request -  New user was able to register');
+    Users.findOne({Username: req.body.Username})
+        .then((user) => {
+          if (user) {
+              return res.status(400).send(req.body.Username + 'already exists');
+          } else {
+              Users
+                  .create({
+                      Username: req.body.Username,
+                      Password: req.body.Password,
+                      Email: req.body.Email,
+                      Birthday: req.body.Birthday
+                  })
+                  .then((user) =>{res.status(201).json(user)})
+              .catch((error) => {
+                  console.error(error);
+                  res.status(500).send('Error: ' + error);
+              })
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        });
 });
 
-// Update the username
+// Update the username               WORKING
 app.put('/users/:id', (req, res) => {
-    res.send('Sucesscul PUT request - User changes username');
+    Users.findByIdAndUpdate({_id: req.params.id},
+    {$set:
+       {
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
+        }
+      },
+      {new: true}, //This line makes sure that the updated document is returned
+      (err, updatedUser) => {
+          if(err) {
+              console.error(err);
+              res.status(500).send('Error: ' + err);
+          } else {
+              res.json(updatedUser);
+          }
+      });
 });
 
 //Update the favlist
-app.put('/users/:id/favs/:dramaId', (req, res) => {
-    res.send('Sucesscul PUT request - User adds a drama to fav list');
+app.post('/users/:id/favs/:dramaId', (req, res) => {
+    Users.findByIdAndUpdate({_id: req.params.id},
+    {$push:
+        {FavDramas: req.params.dramaId}
+    },
+    {new: true}, //This line makes sure that the updated document is returend
+    (err, updatedUser) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        } else {
+            res.json(updatedUser);
+        }
+    });
 });
 
 //Delte drama from the favlist
 app.delete('/users/:id/favs/:dramaId', (req, res) => {
-    res.send('Sucesscul DELETE - User deletes a drama from fav list');
-});
+    Users.findByIdAndUpdate({_id: req.params.id},
+    {$pull:
+        {FavDramas: req.params.dramaId}
+    },
+    {new: true},
+    (err, updatedUser) => {
+        if(err) {
+            console.error(err);
+            res.status(500).send('Error: ' + err);
+        } else {
+          res.json(updatedUser);
+        }
+    });
+});;
 
 // Delete user
 app.delete('/users/:id', (req, res) => {
-    res.send('Sucesscul DELETE request - User deregisters');
-})
+    Users.findByIdAndRemove({_id: req.params.id})
+      .then((user) => {
+          if(!user) {
+              res.status(400).send(req.params.id + ' was not found');
+          } else {
+              res.status(200).send('Your account was deleted.');
+          }
+      })
+      .catch((err) => {
+          console.error(err);
+          res.status(500).send('Error: ' + err);
+      });
+});
 
 // Error response
 app.use((err, req, res, next) => {
