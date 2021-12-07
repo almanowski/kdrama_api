5a2dc9dc7f15a17587b003b9a54f0d5d4fe7f7d3
Anna Manowski <anna.l.manowski@gmail.com>
2021-12-07 12:02:18 +0100

    Delete "in-memory" array

--- index.js
+++ index.js
@@ -14,65 +14,6 @@ const KDramas= Models.KDrama,
     Genres = Models.Genre;
 
 
-// Created JSON object to carry kdrama data
-let topKoreanDramas = [
-    {
-        title: 'Guardian: The Lonely and Great God',
-        dramaId: 1,
-        episode: 16,
-        releaseYear: 2016,
-        descreption: 'Kim Shin, a decorated military general from the Goryeo...',
-        genre: ['fantasy', 'romance'],
-        director: ['Lee Eung-Bok'],
-        writer: ['Kim Eun-Sook'],
-        imgURL: 'https://asianwiki.com/images/b/b2/Goblin-p04.jpg'
-    },
-    {
-        title: 'Oh my Venus',
-        dramaId: 2,
-        episode: 16,
-        releaseYear: 2015,
-        descreption: 'Lorem ipsum dolor sit amet, ...',
-        genre: ['comedy', 'romance', 'drama'],
-        director: ['Kim Hyung-suk'],
-        writer: ['Kim Eun-Ji'],
-        imgURL: 'https://'
-    },
-    {
-        title: 'W: Two worlds apart',
-        dramaId: 3,
-        episode: 16,
-        releaseYear: 2016,
-        descreption: 'Lorem ipsum dolor sit amet, ...',
-        genre: ['romance', 'comedy', 'fantasy', 'action'],
-        director: ['Jung Dae-Yoon'],
-        writer: ['Song Jae-Jung'],
-        imgURL: 'https://'
-    },
-    {
-        title: 'Suspicious Partner',
-        dramaId: 4,
-        episode: 40,
-        releaseYear: 2017,
-        descreption: 'Lorem ipsum dolor sit amet, ...',
-        genre: ['romance', 'comedy', 'legal', 'crime'],
-        director: ['Park Sun-Ho'],
-        writer: ['Kwon Ki-Young'],
-        imgURL: 'https://'
-    },
-    {
-        title: 'Love in the Moonlight',
-        dramaId: 5,
-        episode: 18,
-        releaseYear: 2016,
-        descreption: 'Lorem ipsum dolor sit amet, ...',
-        genre: ['romance', 'comedy', 'historical'],
-        director: ['Kim Sung-Yoon', 'Baek Sang-Hoon'],
-        writer: ['Kim Min-jung', 'Im Ye-jin'],
-        imgURL: 'https://'
-    }
-];
-
 // GENERAL
 // Logs time, method, url path, response code, numb of charac of res that was sent back
 app.use(morgan('common'));
