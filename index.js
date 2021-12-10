c809fa264ba6af5ed10dd57ce9976979025685a5
Anna Manowski <anna.l.manowski@gmail.com>
2021-12-10 10:02:06 +0100

    Update function

--- index.js
+++ index.js
@@ -236,6 +236,7 @@ app.use((err, req, res, next) => {
 });
 
 // Listen to request
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+  console.log('Listening on Port ' + port);
 });
