43019199f363defc6f85589d362eae5e7f3cf49a
Anna Manowski <anna.l.manowski@gmail.com>
2021-12-10 09:56:41 +0100

    Add express-validator

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const express = require('express'),
     Models = require('./models.js'),
     cors = require('cors');
 
+const { check, validationResult } = require('express-validator');
+
 const KDramas= Models.KDrama,
     Users = Models.User,
     Genres = Models.Genre;
@@ -111,31 +113,44 @@ app.get('/directors/:name', passport.authenticate('jwt', {session: false}),
 
 //USER
 // Adds data for a new user !!!!! Doesn't show Birthday!!!!!!!!!!!!
-app.post('/users', (req, res) => {
-    let hashedPassword = Users.hashPassword(req.body.Password);
-    Users.findOne({Username: req.body.Username})
-        .then((user) => {
-          if (user) {
-              return res.status(400).send(req.body.Username + 'already exists');
-          } else {
-              Users
-                  .create({
-                      Username: req.body.Username,
-                      Password: hashedPassword,
-                      Email: req.body.Email,
-                      Birthday: req.body.Birthday
+app.post('/users',
+    [
+        check('Username', 'Username is required').isLength({min: 5}),
+        check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+        check('Password', 'Password is required').not().isEmpty(),
+        check('Email', 'Email does not appear to be valid').isEmail()
+    ], (req, res) => {
+        // Check the validation object for errors
+        let errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(422).json({errors: errors.array() });
+        }
+
+        let hashedPassword = Users.hashPassword(req.body.Password);
+        Users.findOne({Username: req.body.Username})
+            .then((user) => {
+              if (user) {
+                  return res.status(400).send(req.body.Username + 'already exists');
+              } else {
+                  Users
+                      .create({
+                          Username: req.body.Username,
+                          Password: hashedPassword,
+                          Email: req.body.Email,
+                          Birthday: req.body.Birthday
+                      })
+                      .then((user) =>{res.status(201).json(user)})
+                  .catch((error) => {
+                      console.error(error);
+                      res.status(500).send('Error: ' + error);
                   })
-                  .then((user) =>{res.status(201).json(user)})
-              .catch((error) => {
-                  console.error(error);
-                  res.status(500).send('Error: ' + error);
-              })
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send('Error: ' + error);
-        });
+              }
+            })
+            .catch((error) => {
+              console.error(error);
+              res.status(500).send('Error: ' + error);
+            });
 });
 
 // Update the username               WORKING
