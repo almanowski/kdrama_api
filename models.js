b612d609273e180c5ad515140dcb5bb8cb3cb902
Anna Manowski <anna.l.manowski@gmail.com>
2021-12-10 13:59:46 +0100

    Fix typo

--- models.js
+++ models.js
@@ -30,7 +30,7 @@ userSchema.statics.hashPassword = (password) => {
 };
 
 userSchema.methods.validatePassword = function(password) {
-    return bcrypt.compareSync(password, this.Passwords);
+    return bcrypt.compareSync(password, this.Password);
 };
 
 let genreSchema = mongoose.Schema({
