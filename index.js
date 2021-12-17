b8efe2adb98af0d5fdaf19fea9d83e02df4adf80
Anna Manowski <anna.l.manowski@gmail.com>
2021-12-17 08:50:10 +0100

    Update cors

--- index.js
+++ index.js
@@ -28,7 +28,18 @@ app.use(morgan('common'));
 app.use(express.static('public'));
 
 // Allows all domains to make a request to the API
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234']
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1){// If a specifig origin isn't found on the list of origins
+            let message ='The CORS policy for this application doesn\'t allow access from origin ' + origin;
+            return callback(new Error(message ), false);
+        }
+        return callback(null, true);
+    }   
+}));
 
 // Authentication
 let auth = require('./auth.js')(app);
