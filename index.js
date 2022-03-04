1b6d777ac7aa9e57d9c28663266fb3c7ec9b31a3
Anna Manowski <anna.l.manowski@gmail.com>
2022-03-04 18:08:30 +0100

    Add populate

--- index.js
+++ index.js
@@ -168,6 +168,7 @@ app.post('/users',
 app.get('/users/:Username', passport.authenticate('jwt', {session: false}),
 (req, res) => {
     Users.findOne({Username: req.params.Username})
+    .populate('KDrama')
     .then((user) => {
         res.status(201).json(user);
     })
